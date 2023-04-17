ed37fb2f6c8675cafdc252dee56148f8ec09069e
Leonardovallejos1 <87510327+Leonardovallejos1@users.noreply.github.com>
2023-04-17 00:08:04 -0300

    Add files via upload

---
+++ test0.py
@@ -0,0 +1,5 @@
+peso=int(input("ingrese su peso en kg: "))
+estatura=int(input("ingrese cual es su altura en metros:"))#si no uso int va a concatenar como strings
+masaCorporal=peso+estatura
+
+print("Tu indice de masa corporal es "+ str(masaCorporal))#aca tengo que usar str sino no me lo deja imprimir
